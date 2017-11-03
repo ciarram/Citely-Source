b043168a486104468e84be7208f6d2a99ef09164
Julia McGee <JuliaMcGee@Kenneths-MacBook-Pro.local>
2017-11-03 10:50:03 -0400

    login form displays on page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -8,21 +8,24 @@ class LoginForm extends Component {
         return(
             <div>
                 <Container>
+                <form>
                     <Row>
-                        <Input>
-                            Username
-                        </Input>
+                        <Input
+                        name="username"
+                        placeholder="username (required)"
+                        />
                     </Row>
                     <Row>
-                        <Input>
-                            Password
-                        </Input>
+                        <Input
+                        name="password"
+                        type="password"
+                        placeholder="password (required)"
+                        />
                     </Row>
                     <Row>
-                        <Input>
-                            <LoginBtn />
-                        </Input>
+                     <LoginBtn> Login </LoginBtn>
                     </Row>
+                    </form>
                 </Container>
             </div>
         );
