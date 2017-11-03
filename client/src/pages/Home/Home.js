802715bfd6cf9210c60bc3c507876327a874614c
Ciarra Murphy <mciarra@ymail.com>
2017-11-02 23:24:01 -0400

    changed file in html file

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
+import { Col, Row, Container } from "../../components/Grid";
 import Nav from "../../components/Nav";
 
 class Home extends Component {
