e4474761847ac4894cae309a08bd3e3e999f1b95
Ciarra Murphy <mciarra@ymail.com>
2017-11-06 10:50:32 -0500

    added code to home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,8 +10,51 @@ import { Col, Row, Container, Article, Section } from "../../components/Grid";
 // C.M. - Setup the general outline of the Home page for the Book, Article and Project sections
 class Home extends Component {
     state = {
-
+        booktitle: ""
     }
+
+    // componentDidMount() {
+    //     this.loadBookQuotes();
+    //   }
+    
+    //   // Loads all books  and sets them to this.state.books
+    //   loadBookQuotes = () => {
+    //     API.getBooks()
+    //       .then(res =>
+    //         this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+    //       )
+    //       .catch(err => console.log(err));
+    //   };
+    
+    //   // Deletes a book from the database with a given id, then reloads books from the db
+    //   deleteBook = id => {
+    //     API.deleteBook(id)
+    //       .then(res => this.loadBooks())
+    //       .catch(err => console.log(err));
+    //   };
+    
+    //   // Handles updating component state when the user types into the input field
+    //   handleInputChange = event => {
+    //     const { name, value } = event.target;
+    //     this.setState({
+    //       [name]: value
+    //     });
+    //   };
+    
+    //   // When the form is submitted, use the API.saveBook method to save the book data
+    //   // Then reload books from the database
+    //   handleFormSubmit = event => {
+    //     event.preventDefault();
+    //     if (this.state.title && this.state.author) {
+    //       API.saveBook({
+    //         title: this.state.title,
+    //         author: this.state.author,
+    //         synopsis: this.state.synopsis
+    //       })
+    //         .then(res => this.loadBooks())
+    //         .catch(err => console.log(err));
+    //     }
+    //   };
     // C.M. - should return the Books and Article sections on the left side of the screen and current project on the right side of the screen
     render() {
         return(
