53301eb4e661a67b741fbbb1e49673760f1c981d
Julia McGee <juliamcgee@Julias-MacBook-Pro.local>
2017-11-06 21:24:44 -0500

    all routes working when tested in Postman

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,5 @@
 const db = require("../models");
 
-// Defining methods for the booksController
 module.exports = {
   // find all projects associated with a user 
   findAll: function (req, res) {
@@ -14,7 +13,17 @@ module.exports = {
     }
     else { res.json({ error: "Please login", statusCode: 401 }) }
   },
-  //
+  //find all quotes associated with a particular project.(Same route can be recreated for outline and article quote schemas. The only difference is we would use db.Bookquote and db.Outline. Can add later and attach to buttons on the front end to display all data associated with one project. jm)
+  findAllInProject: function (req, res) {
+    if (req.user) {
+      db.Bookquote 
+        .find({projectId : req.params.id})
+        // .sort({ date: -1 })
+        .then(dbModel => res.json({results: dbModel, sess: req.session}))
+        .catch(err => res.status(422).json(err, console.log(err)));
+    }
+    else { res.json({ error: "Please login", statusCode: 401 }) }
+  },
   findById: function (req, res) {
     if (req.user) {
       db.Project
@@ -44,7 +53,7 @@ module.exports = {
     if (req.user) {
       db.Bookquote
         // .findById(req.params.id)
-        .create({title : req.body.title, author : req.body.author, publisher : req.body.publisher, pubDate : req.body.pubDate, pageNum : req.body.pageNum, quote : req.body.quote})
+        .create({title : req.body.title, author : req.body.author, publisher : req.body.publisher, pubDate : req.body.pubDate, pageNum : req.body.pageNum, quote : req.body.quote, projectId : req.body.projectId })
         // projectId : req.params.id
         // .create({projectId : req.body.projectId})
         .then(dbModel => res.json({results: dbModel, sess: req.session}))
@@ -58,7 +67,7 @@ module.exports = {
     console.log("inside createartquote function");
     if (req.user) {
       db.Articlequote
-        .create(req.body)
+        .create({articleName : req.body.articleName, author : req.body.author, siteName : req.body.siteName, version : req.body.version, number : req.body.number, publisher : req.body.publisher, pubDate : req.body.pubDate, url : req.body.url, accessDate : req.body.accessDate, quote : req.body.quote, projectId : req.body.projectId })
         .then(dbModel => res.json({results: dbModel, sess: req.session}))
         .catch(err => res.status(422).json(err));
 
@@ -77,10 +86,10 @@ module.exports = {
     }
     else { res.json({ error: "Please login", statusCode: 401 }) }
   },
-
-  update: function (req, res) {
+//update book quote
+  updatebookq: function (req, res) {
     if (req.user) {
-      db.Project
+      db.Bookquote
         .findOneAndUpdate({ _id: req.params.id }, req.body)
         .then(dbModel => res.json({results: dbModel, sess: req.session}))
         .catch(err => res.status(422).json(err));
@@ -88,14 +97,26 @@ module.exports = {
     }
     else { res.json({ error: "Please login", statusCode: 401 }) }
   },
-  remove: function (req, res) {
+  //delete book quote
+  deletebookq: function (req, res) {
     if (req.user) {
-      db.Project
-        .findById({ _id: req.params.id })
+      db.Bookquote
+        .findById({ _id:"5a01073dc633b807666f8da1"})
         .then(dbModel => dbModel.remove())
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     }
     else { res.json({ error: "Please login", statusCode: 401 }) }
+  },
+//delete project
+deleteproject: function (req, res) {
+  if (req.user) {
+    db.Project
+      .findById({ _id:"5a01073dc633b807666f8da1"})
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
+  else { res.json({ error: "Please login", statusCode: 401 }) }
+}
 };
