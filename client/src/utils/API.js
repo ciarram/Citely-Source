938659c26efbfbbecbd6faa15ec8cf0be5d7f32c
Julia McGee <juliamcgee@Julias-MacBook-Pro.local>
2017-11-06 23:23:41 -0500

    cleaning up routing files and adding detailed notes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,28 +2,28 @@ import axios from "axios";
 
 export default {
 
-//create new project 
+//create new project and save to db
   createProject: function(newProject) {
     console.log("reaching createProject front end")
     return axios.post("/api/projects/newproject", newProject);
   },
+//create new book quote and save to db
   createbQuote: function(newbookquote) {
     console.log("reaching createbQuote front end")
-    return axios.post("/api/projects/newbquote", newbookquote);
+    return axios.post("/api/projects/home/:id", newbookquote);
   },
+//create new article quote and save to db
   createArtQuote: function(newartquote) {
     console.log("reaching createArtQuote front end")
     return axios.post("/api/projects/newbquote", newartquote);
   },
+  //create new outline - not sure about this route but I think we need it to save info to db.
   createOutline: function(newoutline) {
     console.log("reaching createOutline front end")
     return axios.post("/api/projects/newoutline", newoutline);
   },
-  getProjects: function() {
-    return axios.get("/api/projects");
-  },
-  // Gets all the projects with the given user id
-  getProject: function(id) {
+  // Gets all the projects with the given user id --not sure about the id param in route file, must test on front end
+  getProjects: function(id) {
     return axios.get("/api/projects/" + id);
   },
   // Deletes the book with the given id
@@ -31,9 +31,6 @@ export default {
     return axios.delete("/api/projects/" + id);
   },
   // Saves a book to the database
-  saveProject: function(projectData) {
-    return axios.post("/api/projects", projectData);
-  },
 
   login: function(userData) {
     console.log("LOOKING FOR LOGIN INFO", userData)
@@ -49,3 +46,11 @@ export default {
     return axios.post("/api/auth/register", userData);  //TEST got rid of /api at beginning of route
   },
 };
+
+  // getProjects: function() {
+  //   return axios.get("/api/projects/:id");
+  // },
+
+  // saveProject: function(projectData) {
+  //   return axios.post("/api/projects", projectData);
+  // },
--- routes/api/projects.js
+++ routes/api/projects.js
@@ -2,8 +2,16 @@ const router = require("express").Router();
 const projectController = require("../../controllers/projectController");
 
 // Matches with "/api/projects", must include in URL
+
+//**ROUTE THAT BRINGS US TO PROJECTS PAGE**
+//use when bringing up all projects that belong to a user -- have to figure out how to address this on front end because I am unclear how the ":id" works when it's not specifically referenced in the url. It might be best to hard code a user here.
+router.route("/projects/:id")
+.get(projectController.findAll)
 //use when creating new project -- on click of  new project button in form
-router.route("/newproject")
+.post(projectController.create);
+
+
+
 .post(projectController.create);
 //use when creating a new book quote -- on click of new book quote button in form 
 router.route("/newbquote")
@@ -17,11 +25,6 @@ router.route("/newoutline")
 //don't think we need this one - but keeping it around just in case.
 router.route("/:id")
 .get(projectController.findById)    
-
-//**ROUTE THAT BRINGS US TO PROJECTS PAGE**
-//use when bringing up all projects that belong to a user -- have to figure out how to address this on front end because I am unclear how the ":id" works when it's not specifically referenced in the url. It might be best to hard code a user here.
-router.route("/projects/:id")
-.get(projectController.findAll)
 
 //**ROUTE THAT BRINGS US TO HOME PAGE**
 //use this to reach just one project - this should route us to our homepage. Once again , unsure of how ":id" translates to the front end so we will test it. We can always hardcode here too. 
@@ -46,5 +49,7 @@ router.route("/home/:id/deleteproject")
 //   .get(booksController.findById)
 //   .put(booksController.update)
 //   .delete(booksController.remove);
+
+//router.route("/newproject")
 
 module.exports = router;
