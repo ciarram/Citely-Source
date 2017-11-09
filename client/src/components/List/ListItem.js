1ddca6a10b954d1a0482cd5925e669e88152c8c2
Ciarra Murphy <mciarra@ymail.com>
2017-11-09 14:03:34 -0500

    more formatting with MUI on the pages

--- client/src/components/List/ListItem.js
+++ client/src/components/List/ListItem.js
@@ -1,6 +1,12 @@
 import React from "react";
+import Panel from 'muicss/lib/react/panel';
 
-export const ListItem = props =>
-  <li className="list-group-item">
-    {props.children}
-  </li>;
+export const ListItem = props => (
+  <Panel>
+  {props.children}
+  </Panel>
+  
+  // <li className="list-group-item">
+    // {props.children}
+  // </li>
+);
